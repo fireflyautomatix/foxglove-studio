c56c31d9600536811d57d9f500c4811ba35188e3
Miles Egan <93935560+foxymiles@users.noreply.github.com>
2023-05-23 10:16:41 +0700

    lint fix

--- packages/studio-base/src/players/TopicMappingPlayer/mapping.test.ts
+++ packages/studio-base/src/players/TopicMappingPlayer/mapping.test.ts
@@ -2,8 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { RegisterTopicMapperArgs } from "@foxglove/studio-base/../../studio/src";
-import { GlobalVariables } from "@foxglove/studio-base/hooks/useGlobalVariables";
+import { RegisterTopicMapperArgs } from "@foxglove/studio";
 import {
   MappingInputs,
   mapPlayerState,
